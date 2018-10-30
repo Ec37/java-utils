54a0447c19f2fd469cadc4acdab98b1331372f04
ec37 <veve0752@sina.com>
2018-10-30 19:36:58 +0800

    init project

---
+++ src/main/java/com/cnuc/base/BaseOpt.java
@@ -0,0 +1,122 @@
+package com.cnuc.base;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * FileUtils
+ */
+public class BaseOpt {
+
+    /**
+     * 复制文件到指定目录
+     * 
+     * @param config   配置文件读取到的文件库地址
+     * @param file     文件
+     * @param request  HttpServletRequest
+     * @param folder   文件夹名称
+     * @param fileName 需要保存的文件名
+     * @return
+     */
+    public static String fileCopyToPath(String config, MultipartFile file, HttpServletRequest request, String folder,
+            String fileName) {
+        String extension = getExtension(file.getOriginalFilename());
+        // 获得物理地址
+        String pathRoot = config + folder + System.getProperty("file.separator") + fileName + "." + extension;
+        // 生成相对的URL地址
+        String url = folder + "/" + fileName + "." + extension;
+        // 创建文件实例
+        File tempFile = new File(pathRoot);
+
+        // 判断父级目录是否存在，不存在则创建
+        if (!tempFile.getParentFile().exists()) {
+            tempFile.getParentFile().mkdirs();
+        }
+        try {
+            // 将接收的文件保存到指定文件中
+            file.transferTo(tempFile);
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    /**
+     * 删除文件处理
+     * 
+     * @param request
+     * @param url
+     * @return
+     */
+    public static String delFileUtils(String config, HttpServletRequest request, String url) {
+        // 创建文件实例
+        File tempFile = new File(config + url);
+        try {
+            // 删除
+            tempFile.delete();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    /**
+     * 获取文件后缀名
+     * @param filename
+     * @return
+     */
+    public static String getExtension(String filename) {
+        if (filename == null) {
+            return null;
+        }
+
+        int extensionPos = filename.lastIndexOf(".");
+        int lastUnixPos = filename.lastIndexOf("/");
+        int lastWindowsPos = filename.lastIndexOf("\\");
+        int lastSeparator = Math.max(lastUnixPos, lastWindowsPos);
+        //  斜杠比.的位置靠右，说明文件名右问题
+        int index =  lastSeparator > extensionPos ? -1 : extensionPos;
+
+
+        if (index == -1) {
+            return "";
+        } else {
+            return filename.substring(index + 1);
+        }
+    }
+
+
+    /**
+     * 获取字节数组
+     */
+    public static byte[] getBytes(String filePath) {
+		byte[] buffer = null;
+		try {
+			File file = new File(filePath);
+			FileInputStream fis = new FileInputStream(file);
+			ByteArrayOutputStream bos = new ByteArrayOutputStream(1000);
+			byte[] b = new byte[1000];
+			int n;
+			while ((n = fis.read(b)) != -1) {
+				bos.write(b, 0, n);
+			}
+			fis.close();
+			bos.close();
+			buffer = bos.toByteArray();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return buffer;
+	}
+}
---
+++ src/main/java/com/cnuc/controller/FileController.java
@@ -0,0 +1,94 @@
+package com.cnuc.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.cnuc.base.BaseOpt;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+@RestController
+@RequestMapping
+@CrossOrigin(origins = "*", maxAge = 3600, methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,RequestMethod.DELETE })
+public class FileController {
+	@Value("${file.path}")
+	private String path;
+
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	public Map<String,String> uoloadFile(HttpServletRequest request) throws Exception {
+		Map<String, String> results = new HashMap<String, String>();
+		MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+		Iterator<String> iter = multiRequest.getFileNames();
+		String folder = request.getParameter("folder");
+		if(StringUtils.isEmpty(folder)){
+			folder = "test";
+		}
+		int i = 0;
+		while (iter.hasNext()) {
+		// 一次遍历所有文件
+		MultipartFile file = multiRequest.getFile(iter.next().toString());
+			if (file != null) {
+				//文件处理start
+				String url = BaseOpt.fileCopyToPath(path, file, request, folder,UUID.randomUUID().toString().replaceAll("-", ""));
+				results.put("url_"+i, "/s/"+url);
+				results.put("original_"+i,file.getOriginalFilename());
+				//文件处理end
+				}
+			i++;
+		}
+		return results;
+	}
+
+
+	/**
+	 * 显示文件
+	 */
+	@RequestMapping(value = "/s/{folder}/{fileName:.+}", method = RequestMethod.GET)
+	public void show(@PathVariable String folder, @PathVariable String fileName, HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+		
+		if (StringUtils.isEmpty(folder)) {
+			folder = "";
+		}
+		String pathRoot = path + System.getProperty("file.separator") + folder + System.getProperty("file.separator")
+		+ fileName;
+		File file = new File(pathRoot);
+		
+		byte[] bytes = BaseOpt.getBytes(pathRoot);
+		
+		FileInputStream inputStream = new FileInputStream(file);
+		inputStream.close();
+		
+		String name = file.getName();
+		String extension = BaseOpt.getExtension(name).toUpperCase();
+		if("JPEG".equals(extension)||"JPG".equals(extension)||"PNG".equals(extension)||"GIF".equals(extension)||"BMP".equals(extension)) {
+			response.setContentType("image/"+extension.toLowerCase()+"; charset=utf-8");
+		}else if("FLV".equals(extension)||"AVI".equals(extension)||"MP4".equals(extension)||"MOV".equals(extension)) {
+			response.setContentType("video/"+extension.toLowerCase()+"; charset=utf-8");	
+		}
+		
+		OutputStream stream = response.getOutputStream();
+		stream.write(bytes);
+		stream.flush();
+		stream.close();
+	}
+
+
+}

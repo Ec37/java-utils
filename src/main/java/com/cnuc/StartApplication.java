16bd434e317090794be19d0cd2c5c2d12cc458a6
ec37 <mvover@outlook.com>
2018-12-25 23:30:01 +0800

    wss0

--- src/main/java/com/cnuc/StartApplication.java
+++ src/main/java/com/cnuc/StartApplication.java
@@ -1,10 +1,46 @@
 package com.cnuc;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.corundumstudio.socketio.AuthorizationListener;
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.HandshakeData;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.annotation.SpringAnnotationScanner;
 
 @SpringBootApplication
 public class StartApplication {
+	
+	@Value("${wss.service.prot}")
+	private Integer port;
+	
+	@Value("${wss.service.host}")
+	private String host;
+	
+	@Bean
+	public SocketIOServer socketIOServer() {
+		Configuration config = new Configuration();
+		config.setHostname(host);
+		config.setPort(port);
+		config.setAuthorizationListener(new AuthorizationListener() {
+			@Override
+			public boolean isAuthorized(HandshakeData data) {
+				return true;
+			}
+			
+		});
+		final SocketIOServer service = new SocketIOServer(config);
+		return service;
+	}
+	
+	@Bean
+	public SpringAnnotationScanner springAnnotationScanner(SocketIOServer socketIOServer) {
+		return new SpringAnnotationScanner(socketIOServer);
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(StartApplication.class, args);
---
+++ src/main/java/com/cnuc/handler/MessageEventHandler.java
@@ -0,0 +1,43 @@
+package com.cnuc.handler;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.corundumstudio.socketio.AckRequest;
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.annotation.OnConnect;
+import com.corundumstudio.socketio.annotation.OnDisconnect;
+import com.corundumstudio.socketio.annotation.OnEvent;
+
+@Component
+public class MessageEventHandler {
+	
+	private final SocketIOServer server;
+	
+
+	@Autowired
+	public MessageEventHandler(SocketIOServer server) {
+		this.server = server;
+	}
+	
+	@OnConnect
+	public void onConnect(SocketIOServer socketIOServer) {
+		System.out.println("test connect");//TODO:记录用户登陆状态
+	}
+	
+	@OnDisconnect
+	public void onDisconnect(SocketIOClient  client) {
+		System.out.println("test disconnect");//TODO:记录用户短线状态
+	}
+	
+	@OnEvent(value="messageevent")
+	public void onEvent(SocketIOClient client,AckRequest request,Map<String,String> map) {
+		
+	}
+	
+	
+
+}
